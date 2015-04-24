fbeb80fc45b5f45f660e737228fdc19bc2a349d1
erayalakese <erayalakese@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-24 02:30:42 +0000

    tagging v4.1.2
    git-svn-id: https://plugins.svn.wordpress.org/slack-integration/tags/1.4.2@1144050 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tests/test-plugin.php
@@ -0,0 +1,47 @@
+<?php
+
+class SlackPlugin_Test extends WP_UnitTestCase {
+
+	public $plugin;
+
+	public function setUp()
+	{
+		$this->plugin = new Slack_Plugin();
+	}
+
+	public function testVersion()
+	{
+		$this->assertEquals($this->plugin->getVersion(), "1.4.2");
+	}
+
+	public function test_waiting_comment_hook()
+	{
+		
+	}
+
+	public function test_register_options()
+	{
+		$this->plugin->register_options(array('test'=>'data'));
+		$result = json_decode(get_option('slack_options'));
+		$this->assertObjectHasAttribute('test', $result);
+		$result = null;
+
+		$this->plugin->register_options('data');
+		$result = get_option('slack_options');
+		$this->assertEquals('data', $result);
+
+	}
+
+	public function test_get_options()
+	{
+		$this->plugin->register_options(array('test'=>'data'));
+		$result = $this->plugin->get_options();
+		$this->assertObjectHasAttribute('test', $result);
+		$result = null;
+
+		$this->plugin->register_options('data');
+		$result = $this->plugin->get_options();
+		$this->assertEquals('data', $result);
+	}
+
+}
