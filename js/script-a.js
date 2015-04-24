a9a1a3d3900d7ccd58d44e7d69436583b405af6a
erayalakese <erayalakese@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-24 02:22:26 +0000

    ad js
    git-svn-id: https://plugins.svn.wordpress.org/slack-integration/tags/1.4.1@1144047 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/script-a.js
@@ -0,0 +1,4 @@
+jQuery(document).ready(function() {
+	var html = '<div id="reklam"> <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> <!-- wp-slack Plugin Admin Page --> <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-9792205570091420" data-ad-slot="2441051284" data-ad-format="auto"></ins> <script> (adsbygoogle = window.adsbygoogle || []).push({}); </script> </div>';
+	jQuery("div.wrap").prepend(html);
+});
